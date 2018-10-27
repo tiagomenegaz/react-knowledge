03010d30e7fb98488198f0c8a09d76dce603f945
Tiago Garcia <tiago.tiago22@gmail.com>
2018-10-27 13:28:27 -0300

    add card component to robofriends

---
+++ intro/robofriends/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const card = () => {
+  return (
+    <div>
+      <img alt='photo' src=''/>
+      <div>
+        <h2>Terminator</h2>
+        <p>terminator@skynet.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default card;
