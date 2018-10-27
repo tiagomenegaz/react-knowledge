9abe4a59d6efa32b0cafae6d1db8014aafaf38b3
Tiago Garcia <tiago.tiago22@gmail.com>
2018-10-27 13:59:41 -0300

    add props to card robofriends

--- intro/robofriends/src/components/Card.js
+++ intro/robofriends/src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const card = () => {
+const card = ({ id, name, email }) => {
   return (
-    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='photo' src='https://robohash.org/test?200x200'/>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='photo' src={`https://robohash.org/${ id }?200x200`}/>
       <div>
-        <h2>Terminator</h2>
-        <p>terminator@skynet.com</p>
+        <h2>{ name }</h2>
+        <p>{ email }</p>
       </div>
     </div>
   );
