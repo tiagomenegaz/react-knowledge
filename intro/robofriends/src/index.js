df838bf9ac4cd11057aa626577b5d7005a1e2406
Tiago Garcia <tiago.tiago22@gmail.com>
2018-10-27 14:24:41 -0300

    update index to use cardlist component - robofriends

--- intro/robofriends/src/index.js
+++ intro/robofriends/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './components/Card';
+import CardList from './components/CardList';
 import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-const cardList = <div>
-  <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-  <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-  <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-</div>
-
-ReactDOM.render(cardList, document.getElementById('root'));
+ReactDOM.render(<CardList elements={ robots } />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
