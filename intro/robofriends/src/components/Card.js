6ebf40b670ca6b1e5d3dedd49fde600a20f5dc4d
Tiago Garcia <tiago.tiago22@gmail.com>
2018-10-27 13:37:51 -0300

    add basic style to card robofriends

--- intro/robofriends/src/components/Card.js
+++ intro/robofriends/src/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const card = () => {
   return (
-    <div>
-      <img alt='photo' src=''/>
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='photo' src='https://robohash.org/test?200x200'/>
       <div>
         <h2>Terminator</h2>
         <p>terminator@skynet.com</p>
