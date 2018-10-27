c5f8c6a6d19479564af91565539a06999a8fd8b2
Tiago Garcia <tiago.tiago22@gmail.com>
2018-10-27 13:38:08 -0300

    add multiple cards to robofriends

--- intro/robofriends/src/index.js
+++ intro/robofriends/src/index.js
@@ -5,7 +5,9 @@ import Card from './components/Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+const cardList = <div><Card /><Card /><Card /></div>
+
+ReactDOM.render(cardList, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
