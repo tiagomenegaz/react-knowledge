c659bd00080e3c5044470b1e0fc0694690d7d82f
Tiago Garcia <tiago.tiago22@gmail.com>
2018-10-27 13:19:59 -0300

    refactor class syntax to use function syntax at robofriends

--- intro/robofriends/src/components/Hello.js
+++ intro/robofriends/src/components/Hello.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Hello.css';
 
-class Hello extends Component {
-  render() {
-    return (
-      <div className='f1 tc'>
-        <h1>Hello World!</h1>
-        <p>{this.props.greetings}</p>
-      </div>
-    );
-  }
-}
+const Hello = (props) => {
+  return (
+    <div className='f1 tc'>
+      <h1>Hello World!</h1>
+      <p>{props.greetings}</p>
+    </div>
+  );
+};
 
 export default Hello;
