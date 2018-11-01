9ff4fda7edad80c0f8e16e56b29d5882e8f42589
Tiago Garcia <tiago.tiago22@gmail.com>
2018-10-31 22:47:22 -0300

    refactor app js to containers folder

--- intro/robofriends/src/containers/App.js
+++ intro/robofriends/src/containers/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import { robots } from '../robots';
 
 class App extends Component {
