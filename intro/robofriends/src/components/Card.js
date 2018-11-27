3e859ac1550237e31d2c9c1aad62ad2a7d77c777
Tiago Garcia <tiago.tiago22@gmail.com>
2018-11-26 23:17:22 -0200

    fix img tag warning

--- intro/robofriends/src/components/Card.js
+++ intro/robofriends/src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const card = ({ id, name, email }) => {
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='photo' src={`https://robohash.org/${ id }?200x200`}/>
+      <img alt='user pic' src={`https://robohash.org/${ id }?200x200`}/>
       <div>
         <h2>{ name }</h2>
         <p>{ email }</p>
