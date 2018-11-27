408be942a264f6c1668d9ad6550208ca2491e87d
Tiago Garcia <tiago.tiago22@gmail.com>
2018-11-27 00:40:06 -0200

    add connection to app

--- intro/robofriends/src/containers/App.js
+++ intro/robofriends/src/containers/App.js
@@ -1,25 +1,36 @@
 import './App.css';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchField: '',
     };
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
-  }
-
   render() {
-    const { robots, searchField } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
@@ -34,7 +45,7 @@ class App extends Component {
       return (
         <div className='tc'>
           <h1>Robofriends</h1>
-          <SearchBox searchChange={ this.onSearchChange } />
+          <SearchBox searchChange={ onSearchChange } />
           <Scroll>
             <ErrorBoundry>
               <CardList elements={ filteredRobots } />
@@ -53,4 +64,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- intro/robofriends/src/reducers.js
+++ intro/robofriends/src/reducers.js
@@ -5,6 +5,8 @@ const initialState = {
 };
 
 export const searchRobots = (state=initialState, action={}) => {
+  console.log(action.type);
+
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });
